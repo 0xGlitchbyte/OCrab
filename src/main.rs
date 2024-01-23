48c485c3045ed513c463903cdfef3ed45a6e20fb
0xGlitchbyte <49317853+0xGlitchbyte@users.noreply.github.com>
2024-01-23 08:54:44 -0500

    needs testing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fmt::Display;
 use std::fs::File;
 use std::io::Read;
-use syn::{Expr, ExprLit, Item, ItemConst, Lit};
+use syn::{Expr, ExprLit, Item, ItemConst, Lit, Type};
 
 //use syn::File;
 
@@ -9,7 +9,7 @@ use syn::{Expr, ExprLit, Item, ItemConst, Lit};
 enum OCaml {
     Let {
         name: String,
-        //type_: String,
+        ty: Option<String>,
         value: String,
     },
 }
@@ -17,7 +17,9 @@ enum OCaml {
 impl Display for OCaml {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            OCaml::Let { name, value } => write!(f, "let {} = {}", name, value),
+            OCaml::Let { name, ty, value } => {
+                write!(f, "let {} : {} = {}", name, ty, value)
+            }
         }
     }
 }
@@ -44,6 +46,19 @@ impl Display for OCamlLiteral {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             OCamlLiteral::Number(int) => write!(f, "{}", int),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum OCamlType {
+    Ty(String),
+}
+
+impl Display for OCamlType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            OCamlType::Ty(ty) => write!(f, "{}", ty),
         }
     }
 }
@@ -77,10 +92,12 @@ fn rust_item_to_ocaml_item(item: syn::Item) -> Option<OCaml> {
         Item::Const(ItemConst {
             ident: name,
             expr: value,
+            ty: ty,
             ..
         }) => Some(OCaml::Let {
             name: format!("{}", name),
             value: format!("{}", rust_expr_to_ocaml_expr(&value).unwrap()),
+            ty: format!("{}", extract_type_from_rust_ast(&ty)),
         }),
         _ => None,
     }
@@ -98,6 +115,20 @@ fn rust_expr_to_ocaml_expr(expr: &Expr) -> Option<OCamlExpr> {
 fn rust_literal_to_ocaml_literal(lit: &Lit) -> Option<OCamlLiteral> {
     match lit {
         Lit::Int(int) => Some(OCamlLiteral::Number(format!("{}", int))),
+        _ => None,
+    }
+}
+
+fn extract_type_from_rust_ast(ty: &Type) -> Option<String> {
+    // Type -> Path -> Segements -> PathSegment -> Ident(String)
+    // Type::TypePath::Path::Punctuated{PathSegment{Ident}}
+    match ty {
+        Type::Path(type_path) => {
+            let path = &type_path.path;
+            let seg = path.segments.last()?;
+            let ident = &seg.ident;
+            return Some(ident.to_string());
+        }
         _ => None,
     }
 }
