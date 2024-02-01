916993ec5447399ed469922ef7c47b84102f6748
Amit Singh <amitsingh19975@gmail.com>
2024-02-02 00:22:36 +0530

    Add Debug, PartialEq, PartialOrd, Ord, Eq traits to enums and tests

--- src/ocaml.rs
+++ src/ocaml.rs
@@ -30,7 +30,7 @@ impl std::fmt::Display for OCamlError {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, PartialOrd, Ord, Eq)]
 pub enum OCaml {
     Let {
         name: String,
@@ -64,7 +64,7 @@ impl OCaml {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, PartialOrd, Ord, Eq)]
 pub enum OCamlExpr {
     Literal(OCamlLiteral),
     Path(Vec<String>),
@@ -72,13 +72,13 @@ pub enum OCamlExpr {
                                 //Struct
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, PartialOrd, Ord, Eq)]
 pub enum OCamlLiteral {
     Integer{ digits: String, width: Option<usize>, is_signed: Option<bool>, is_native: bool },
     Float{ digits: String, width: Option<usize> },
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, PartialOrd, Ord, Eq)]
 pub enum OCamlUnaryExpr {
     Minus(OCamlExpr),
     Deref(OCamlExpr),
@@ -231,5 +231,80 @@ impl From<&syn::LitFloat> for OCamlLiteral {
         }
 
         unreachable!("Unknown suffix: {}", suffix)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn parse_literal_helper(
+        name: &str,
+        ty: &str,
+        digits: &str, 
+        width: Option<usize>, 
+        is_signed: Option<bool>, 
+        is_native: bool, 
+        is_float: bool
+    ) -> OCaml {
+        let lit = if is_float {
+            OCamlLiteral::Float {
+                digits: digits.to_string(),
+                width
+            }
+        } else {
+            OCamlLiteral::Integer {
+                digits: digits.to_string(),
+                width,
+                is_signed,
+                is_native
+            }
+        };
+
+        OCaml::Let {
+            name: name.to_string(),
+            ty: Some(ty.to_string()),
+            value: Some(OCamlExpr::Literal(lit))
+        }
+    }
+
+    #[test]
+    fn test_parse_literal() {
+        let source = r#"
+            const A: c_int = 1u8;
+            const A: c_int = 1u16;
+            const A: c_int = 1u32;
+            const A: c_int = 1u64;
+            const A: c_int = 1usize;
+            const A: c_int = 1i8;
+            const A: c_int = 1i16;
+            const A: c_int = 1i32;
+            const A: c_int = 1i64;
+            const A: c_int = 1isize;
+            const A: c_float = 1f32;
+            const A: c_float = 1f64;
+
+            const B: c_float = 1.0f32;
+            const B: c_float = 1.0f64;
+        "#;
+
+        let syntax = syn::parse_file(source).unwrap();
+        let syntax_items: Vec<OCaml> = syntax.items.iter().map(|item| item.into()).collect();
+        
+        assert_eq!(syntax_items.len(), 14);
+        assert_eq!(syntax_items[0], parse_literal_helper("A", "c_int", "1", Some(8),  Some(false), false, false));
+        assert_eq!(syntax_items[1], parse_literal_helper("A", "c_int", "1", Some(16), Some(false), false, false));
+        assert_eq!(syntax_items[2], parse_literal_helper("A", "c_int", "1", Some(32), Some(false), false, false));
+        assert_eq!(syntax_items[3], parse_literal_helper("A", "c_int", "1", Some(64), Some(false), false, false));
+        assert_eq!(syntax_items[4], parse_literal_helper("A", "c_int", "1", None,     Some(false), true,  false));
+        assert_eq!(syntax_items[5], parse_literal_helper("A", "c_int", "1", Some(8),  Some(true ), false, false));
+        assert_eq!(syntax_items[6], parse_literal_helper("A", "c_int", "1", Some(16), Some(true ), false, false));
+        assert_eq!(syntax_items[7], parse_literal_helper("A", "c_int", "1", Some(32), Some(true ), false, false));
+        assert_eq!(syntax_items[8], parse_literal_helper("A", "c_int", "1", Some(64), Some(true ), false, false));
+        assert_eq!(syntax_items[9], parse_literal_helper("A", "c_int", "1", None,     Some(true ), true,  false));
+        assert_eq!(syntax_items[10], parse_literal_helper("A", "c_float", "1",   Some(32), None, false, true));
+        assert_eq!(syntax_items[11], parse_literal_helper("A", "c_float", "1",   Some(64), None, false, true));
+        assert_eq!(syntax_items[12], parse_literal_helper("B", "c_float", "1.0", Some(32), None, false, true));
+        assert_eq!(syntax_items[13], parse_literal_helper("B", "c_float", "1.0", Some(64), None, false, true));
     }
 }
